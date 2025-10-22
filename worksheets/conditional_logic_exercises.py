e9508b9c0c68829ace4f832befe5c403213a04d7
ribkagezahagn6-svg <ribkagezahagn6@gmail.com>
2025-10-22 06:18:09 -0700

    Add bitwise shift exercises to conditional logic

--- worksheets/conditional_logic_exercises.py
+++ worksheets/conditional_logic_exercises.py
@@ -1,2 +1,16 @@
 print("Conditional Logic Exercises")
-# Practice if/elif/else statements here.
+integer_1=int(input("Enter first number: "))
+shift=int(input("Enter number of shif positions: "))
+print("original number: "+integer_1)
+print ("binary( befor shift):"+ bin(integer_1))
+
+
+left_shift = integer_1<<shift
+print("After left shift "+shift +" positions :")
+print("Decimal :"+left_shift+".")
+print("binary: "+bin(left_shift))
+
+right_shift=integer_1 >> shift
+print("after right shift "+shift +"positions: ")
+print("decimal: "+right_shift) 
+print("binary: "+bin(right_shift))
