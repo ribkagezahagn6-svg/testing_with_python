c4fbc1144d80952ebce09cfe54cc69127eef9aea
ribkagezahagn6-svg <ribkagezahagn6@gmail.com>
2025-10-22 06:06:14 -0700

    Add simple calculator functionality
    Implement a simple calculator that performs basic operations.

--- Problem_3_Simple_Calculator/solution.py
+++ Problem_3_Simple_Calculator/solution.py
@@ -1,1 +1,22 @@
-# your code here
+#problem 3
+print("Quick Calculator")
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator ( +, _, *, /): ")
+num2 = float(input("Enter second number:"))
+if operator == '+' :
+        result = num1 + num2
+        print(f"Result: {result}")
+elif operator == '-' :
+        result = num1 - num2
+        print(f"Result: {result}")
+elif operator == '*' :
+        result = num1 * num2
+        print(f"Result: {result}")
+elif operator == '/':
+   if num2!=0:
+         result= num1/num2
+         print(f"Result:{result}")
+   else:
+        print("Error: ")
+else:
+        print("invalid operater")
