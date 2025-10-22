da9bf33f83fc17632cb1646d0dca6e30d7908d8e
ribkagezahagn6-svg <ribkagezahagn6@gmail.com>
2025-10-22 06:05:33 -0700

    Implement miles to kilometers converter

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,5 @@
-# your code here
+#problem 2
+print("=== MILES TO KILOMETERS CONVERTER ===")
+miles = float(input("Enter distance in miles: "))
+kilometers = miles * 1.60934
+print(f"{miles} miles = {kilometers:2f} ")
